558571f9bf75f41a49ecd4e704ee9d6428225156
orudda <othavio.rudda.cc@gmail.com>
2023-06-18 09:31:18 -0300

    first draft

---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import plotly.graph_objects as go
+
+import math
+
+import pandas as pd
+import json
+pd.set_option('display.max_columns', None)
+
+from io import BytesIO
+from pathlib import Path
+
+import socceraction.spadl as spadl
+import socceraction.spadl.wyscout as wyscout
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+from utils import Utils
+
+# Configurações de página
+st.set_page_config(
+    page_title="Visualização de Dados",
+    page_icon="🎓",
+    layout="wide"
+)
+
+# Título e cabeçalho
+st.title("Visualização de Dados")
+# st.markdown("Bem-vindos ao site da Escola XYZ!")
+
+# Menu de navegação
+menu_options = ["Home", "Contribuições", "Dados", "Visualização Manual"]
+selected_option = st.sidebar.selectbox("Navegação", menu_options)
+
+#Contribuintes
+owners = ["Kaio Na Pica", "Othávio Ruddá"]
+
+utils = Utils()
+
+# Página inicial
+if selected_option == "Home":
+    st.header("Bem-vindo ao trabalho de visualização de dados!")
+    # st.write("Aqui você encontrará informações sobre nossos cursos, professores e muito mais.")
+
+    st.subheader("Contribuintes:")
+    for i in owners:
+        st.write(i)
+
+    st.subheader("Sumário")
+    for i in menu_options:
+        st.write("\t     -"+i)
+
+
+elif selected_option == "Contribuições":
+    st.header("Contribuições")
+    st.write("Nessa página descrevemos as contribuições de cada aluno")
+
+   
+    contributions = {
+        "Contribuinte": owners,
+        # "contribuição": owners,
+        "Contribuição": ["Desenvolveu a visualização 5 e participou do desenvolvimento do  design do site e do processo de deploy do sistema web.",
+                         "Desenvolveu a visualização 1 e 6 e participou do processo de deploy do sistema web.",]
+    }
+    cursos_df = pd.DataFrame(contributions)
+    cursos_df = cursos_df.reset_index(drop=True)
+    st.dataframe(cursos_df)
+
+# Página de contato
+elif selected_option == "Dados":
+    st.header("Dados Utilizados")
+    text = "texto de dados"
+    st.write(text)
+    st.header("Times")
+    st.dataframe(utils.df_teams.head())
+    st.header("Eventos")
+    st.dataframe(utils.df_events.head())
+
+
+# Página de dados manual
+elif selected_option == "Visualização Manual":
+    st.header("Total de Passes")
+    listEvents = ['Head pass', 'Simple pass', 'High pass', 'Smart pass', 'Hand pass']
+    df = utils.countingEvents(listEvents)
+    df['total'] = df.sum(axis=1)
+    df = df.sort_values('total', ascending=False)
+    df['mean'] = df['total']/38
+    st.dataframe(df)
+
+    st.header("Gráfico de Passes")
+    utils.plotChart(title='Média de Passes por Time',x_title='Time',y_title='Média de Passes',x_df=df.index,y_df=df['mean'])
+
+    listEvents = ['Free kick shot', 'Shot']
+    df = utils.countingEvents(listEvents)
+    st.header("Gráfico de Chutes")
+    df['total'] = df.sum(axis=1)
+    df = df.sort_values('total', ascending=False)
+    df['mean'] = df['total']/38
+    utils.plotChart(title='Média de Chutes por Time',x_title='Time',y_title='Média de Chutes',x_df=df.index,y_df=df['mean'])
+
---
+++ utils.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+from tqdm.notebook import tqdm
+
+import math
+
+import pandas as pd
+import json
+pd.set_option('display.max_columns', None)
+
+from io import BytesIO
+from pathlib import Path
+
+import socceraction.spadl as spadl
+import socceraction.spadl.wyscout as wyscout
+import plotly.graph_objects as go
+import streamlit as st
+
+class Utils:
+    def __init__(self):
+        
+        with open('files/teams.json', 'r') as f:
+            data = json.load(f)
+
+        self.df_teams = pd.DataFrame(data)
+
+        with open('files/events_England.json', 'r') as f:
+            data = json.load(f)
+
+        self.df_events = pd.DataFrame(data)
+
+    
+    def dfMergedName(self):
+        return  pd.merge(self.df_events, self.df_teams, left_on='teamId', right_on='wyId', how='left')
+    
+    def countingEvents(self,listEvents):
+        df = self.dfMergedName()
+        df_filtered = df[df['subEventName'].isin(listEvents)]
+        df_total_eventos = pd.pivot_table(df_filtered, index='name', columns='subEventName', aggfunc='size', fill_value=0)
+        return df_total_eventos
+    
+    def sumPasses(self, df):
+        return df.sum(axis=1)
+    
+    def plotChart(self,title, x_title, y_title, x_df, y_df):
+        # Criação do gráfico de barras
+        fig = go.Figure(data=go.Bar(x=x_df, y=y_df))
+
+        # Configurações do layout
+        fig.update_layout(title=title,
+                        xaxis_title=x_title,
+                        yaxis_title=y_title)
+
+        # Exibição do gráfico
+        st.plotly_chart(fig)
+
+
+
